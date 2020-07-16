2ccca98f6675295e6c71655d3719b993c6248452
Shohei Yokoyama <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2020-07-16 17:43:24 +0900

    https

--- assets/vldb2020-program.js
+++ assets/vldb2020-program.js
@@ -1,6 +1,6 @@
 (() => {
     document.addEventListener("DOMContentLoaded", () => {
-        let files = ['https://s.vldb2020.org/VLDB2020session.json', 'http://s.vldb2020.org/VLDB2020timeslot.json', 'https://s.vldb2020.org/VLDB2020paper.json'];
+        let files = ['https://s.vldb2020.org/VLDB2020session.json', 'https://s.vldb2020.org/VLDB2020timeslot.json', 'https://s.vldb2020.org/VLDB2020paper.json'];
         Promise.all(
             files.map((file) => {
                 return fetch(file).then((response) => { return response.json() });
