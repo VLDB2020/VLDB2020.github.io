bb2dc2176d3ad99612c6150a2d99e5984857d5e6
Shohei Yokoyama <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2020-07-17 10:23:49 +0900

    parse ISO datetime

--- assets/vldb2020-program.js
+++ assets/vldb2020-program.js
@@ -1,13 +1,24 @@
 (() => {
-    document.addEventListener("DOMContentLoaded", () => {
-        let files = ['https://s.vldb2020.org/VLDB2020session.json', 'https://s.vldb2020.org/VLDB2020timeslot.json', 'https://s.vldb2020.org/VLDB2020paper.json'];
-        Promise.all(
-            files.map(async(file) => {
-                const response = await fetch(file);
-                return response.json();
-            })
-        ).then((response) => {
-            console.log(response);
+    if (document.getElementById("programFrame") !== null) {
+        document.addEventListener("DOMContentLoaded", () => {
+            let files = [
+                'https://s.vldb2020.org/VLDB2020session.json',
+                'https://s.vldb2020.org/VLDB2020timeslot.json',
+                'https://s.vldb2020.org/VLDB2020paper.json'
+            ];
+            Promise.all(
+                files.map(async(file) => {
+                    const response = await fetch(file);
+                    return response.json();
+                })
+            ).then((response) => {
+                const session = response[0];
+                const timeslot = response[1].map((i) => {
+                    return { slot: i.slot, start: Date.parse(i.start) }
+                });
+                const paper = response[2];
+                console.log(timeselot);
+            });
         });
-    });
+    }
 })();
