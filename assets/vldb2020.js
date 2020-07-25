206a609ae72e155aadbd13edf8595707b96eb235
Shohei Yokoyama <1260486+abarth500@users.noreply.github.com>
2020-07-25 13:02:58 +0900

    formatter (#31)

--- assets/vldb2020.js
+++ assets/vldb2020.js
@@ -1,54 +1,58 @@
 (() => {
-    const e = 'helpdesk';
-    const mail = 'vldb2020.org';
+    const e = "helpdesk";
+    const mail = "vldb2020.org";
 
     let currentPlaybackRate = { value: 1 };
-    document.getElementById("toppage-toggle").addEventListener('change', (event) => {
-        if (event.target.checked) {
-            Cookies.set('focus', 'contents', {
-                expires: 7
-            });
-            anime({
-                targets: currentPlaybackRate,
-                value: 0,
-                easing: 'linear',
-                duration: 3000,
-                update: function() {
-                    document.getElementById("toppage-video").playbackRate = (Math.round(currentPlaybackRate.value * 10)) / 10;
-                }
-            });
-        } else {
-            Cookies.remove('focus');
-            anime({
-                targets: currentPlaybackRate,
-                value: 1,
-                easing: 'linear',
-                duration: 3000,
-                update: function() {
-                    document.getElementById("toppage-video").playbackRate = (Math.round(currentPlaybackRate.value * 10)) / 10;
-                }
-            });
-            anime({
-                targets: '#contents-body',
-                scrollTop: 0,
-                duration: 700,
-                easing: 'easeInOutSine'
-            });
-        }
-    });
-    if (Cookies.get('focus') == "contents") {
-        Cookies.set('focus', 'contents', {
-            expires: 7
+    document
+        .getElementById("toppage-toggle")
+        .addEventListener("change", (event) => {
+            if (event.target.checked) {
+                Cookies.set("focus", "contents", {
+                    expires: 7,
+                });
+                anime({
+                    targets: currentPlaybackRate,
+                    value: 0,
+                    easing: "linear",
+                    duration: 3000,
+                    update: function () {
+                        document.getElementById("toppage-video").playbackRate =
+                            Math.round(currentPlaybackRate.value * 10) / 10;
+                    },
+                });
+            } else {
+                Cookies.remove("focus");
+                anime({
+                    targets: currentPlaybackRate,
+                    value: 1,
+                    easing: "linear",
+                    duration: 3000,
+                    update: function () {
+                        document.getElementById("toppage-video").playbackRate =
+                            Math.round(currentPlaybackRate.value * 10) / 10;
+                    },
+                });
+                anime({
+                    targets: "#contents-body",
+                    scrollTop: 0,
+                    duration: 700,
+                    easing: "easeInOutSine",
+                });
+            }
+        });
+    if (Cookies.get("focus") == "contents") {
+        Cookies.set("focus", "contents", {
+            expires: 7,
         });
         currentPlaybackRate = { value: 0 };
         document.getElementById("toppage-video").playbackRate = 0;
-        document.getElementById('toppage-toggle').checked = true;
+        document.getElementById("toppage-toggle").checked = true;
     }
 
     let submenuInAction = false;
     const submenuToggle = (before, after) => {
-        before = before == 'conference' ? null : before;
-        after = after == 'conference' ? null : after;
+        before = before == "conference" ? null : before;
+        after = after == "conference" ? null : after;
         if (before === null && after === null) {
             return;
         }
@@ -58,119 +62,169 @@
             after = null;
         }
         if (after == null) {
-            sequence.push(new Promise(resolve => {
-                anime({
-                    targets: "#submenu-mask",
-                    opacity: 0.0,
-                    duration: 300,
-                    complete: () => {
-                        document.getElementById("submenu-mask").style.display = "none";
-                        resolve();
-                    }
-                });
-            }));
+            sequence.push(
+                new Promise((resolve) => {
+                    anime({
+                        targets: "#submenu-mask",
+                        opacity: 0.0,
+                        duration: 300,
+                        complete: () => {
+                            document.getElementById(
+                                "submenu-mask"
+                            ).style.display = "none";
+                            resolve();
+                        },
+                    });
+                })
+            );
         }
         if (before != null) {
-            let panelID = 'submenu-panel-' + before;
-            sequence.push(new Promise(resolve => {
-                anime({
-                    targets: '#' + panelID,
-                    translateY: -(document.getElementById(panelID).offsetHeight) + 'px',
-                    easing: 'easeOutExpo',
-                    duration: 1000,
-                    complete: () => {
-                        resolve();
-                    }
-                });
-            }));
+            let panelID = "submenu-panel-" + before;
+            sequence.push(
+                new Promise((resolve) => {
+                    anime({
+                        targets: "#" + panelID,
+                        translateY:
+                            -document.getElementById(panelID).offsetHeight +
+                            "px",
+                        easing: "easeOutExpo",
+                        duration: 1000,
+                        complete: () => {
+                            resolve();
+                        },
+                    });
+                })
+            );
         }
         if (after != null) {
             if (before == null) {
                 document.getElementById("submenu-mask").style.display = "block";
                 sequence.push(
-                    new Promise(resolve => {
+                    new Promise((resolve) => {
                         anime({
                             targets: "#submenu-mask",
                             opacity: 0.8,
                             duration: 300,
                             complete: () => {
                                 resolve();
-                            }
+                            },
                         });
                     })
                 );
             }
-            let panelID = 'submenu-panel-' + after;
-            sequence.push(new Promise(resolve => {
-                document.getElementById(panelID).style.zIndex = 95;
-                anime({
-                    targets: '#' + panelID,
-                    translateY: (document.getElementById(panelID).offsetHeight) + 'px',
-                    easing: 'easeOutQuint',
-                    duration: 1000,
-                    complete: () => {
-                        document.getElementById(panelID).style.zIndex = 90;
-                        resolve();
-                    }
-                });
-            }));
+            let panelID = "submenu-panel-" + after;
+            sequence.push(
+                new Promise((resolve) => {
+                    document.getElementById(panelID).style.zIndex = 95;
+                    anime({
+                        targets: "#" + panelID,
+                        translateY:
+                            document.getElementById(panelID).offsetHeight +
+                            "px",
+                        easing: "easeOutQuint",
+                        duration: 1000,
+                        complete: () => {
+                            document.getElementById(panelID).style.zIndex = 90;
+                            resolve();
+                        },
+                    });
+                })
+            );
         }
-        Promise.all(sequence).then(function(message) {
+        Promise.all(sequence).then(function (message) {
             submenuInAction = false;
         });
-    }
+    };
 
     const maskClear = () => {
         if (submenuInAction) {
             return;
         }
         let before = null;
-        if (document.querySelector('input[name="submenu-active"]:checked') !== null) {
-            before = document.querySelector('input[name="submenu-active"]:checked').value;
+        if (
+            document.querySelector('input[name="submenu-active"]:checked') !==
+            null
+        ) {
+            before = document.querySelector(
+                'input[name="submenu-active"]:checked'
+            ).value;
         }
         let after = null;
-        document.querySelector('input[name="submenu-active"]:checked').checked = false;
+        document.querySelector(
+            'input[name="submenu-active"]:checked'
+        ).checked = false;
         submenuToggle(before, after);
-    }
+    };
 
-    let sublinks = document.querySelectorAll('.submenu-item:not(.link-disable)');
+    let sublinks = document.querySelectorAll(
+        ".submenu-item:not(.link-disable)"
+    );
     for (var c = 0; c < sublinks.length; c++) {
-        sublinks[c].addEventListener('click', maskClear);
+        sublinks[c].addEventListener("click", maskClear);
     }
-    document.getElementById('submenu-mask').addEventListener('click', maskClear);
+    document
+        .getElementById("submenu-mask")
+        .addEventListener("click", maskClear);
 
     const pourContactEmailAddress = () => {
-        const contacts = document.querySelectorAll('.contactaddress');
+        const contacts = document.querySelectorAll(".contactaddress");
         for (var i = 0; i < contacts.length; i++) {
-            contacts[i].innerHTML = '<a href="mai' + 'lto:' + e + '@' + mail + '">' + e + '@' + mail + '</a>';
+            contacts[i].innerHTML =
+                '<a href="mai' +
+                "lto:" +
+                e +
+                "@" +
+                mail +
+                '">' +
+                e +
+                "@" +
+                mail +
+                "</a>";
         }
-    }
+    };
     pourContactEmailAddress();
 
     const updateSocialShare = () => {
-        const socialShare = document.getElementsByClassName('social-icon');
+        const socialShare = document.getElementsByClassName("social-icon");
         for (let c = 0; c < socialShare.length; c++) {
-            socialShare[c].attributes['href'].value = socialShare[c].attributes['x-href'].value.replace("{url}", location.href);
+            socialShare[c].attributes["href"].value = socialShare[c].attributes[
+                "x-href"
+            ].value.replace("{url}", location.href);
         }
-    }
+    };
     updateSocialShare();
 
-    const menuItem = document.getElementsByClassName('submenu-toggle');
+    const menuItem = document.getElementsByClassName("submenu-toggle");
     for (let c = 0; c < menuItem.length; c++) {
-        menuItem[c].addEventListener('click', (e) => {
+        menuItem[c].addEventListener("click", (e) => {
             if (submenuInAction) {
                 e.stopPropagation();
                 return;
             }
             let before = null;
-            if (document.querySelector('input[name="submenu-active"]:checked') !== null) {
-                before = document.querySelector('input[name="submenu-active"]:checked').value;
+            if (
+                document.querySelector(
+                    'input[name="submenu-active"]:checked'
+                ) !== null
+            ) {
+                before = document.querySelector(
+                    'input[name="submenu-active"]:checked'
+                ).value;
             }
-            const checkID = 'submenu-' + e.currentTarget.attributes['x-category'].value;
-            document.getElementById(checkID).checked = !document.getElementById(checkID).checked;
+            const checkID =
+                "submenu-" + e.currentTarget.attributes["x-category"].value;
+            document.getElementById(checkID).checked = !document.getElementById(
+                checkID
+            ).checked;
             let after = null;
-            if (document.querySelector('input[name="submenu-active"]:checked') !== null) {
-                after = document.querySelector('input[name="submenu-active"]:checked').value;
+            if (
+                document.querySelector(
+                    'input[name="submenu-active"]:checked'
+                ) !== null
+            ) {
+                after = document.querySelector(
+                    'input[name="submenu-active"]:checked'
+                ).value;
             }
             submenuToggle(before, after);
         });
@@ -178,25 +232,32 @@
 
     document.onkeydown = (e) => {
         e = e || window.event;
-        if (e.keyCode == '40') {
-            let submenu = document.getElementsByClassName('submenu-active');
+        if (e.keyCode == "40") {
+            let submenu = document.getElementsByClassName("submenu-active");
             for (let c = 0; c < submenu.length; c++) {
                 if (submenu[c].checked) {
                     return;
                 }
             }
-            let el = document.getElementById('contents-body');
-            let height = parseInt(window.getComputedStyle(el).getPropertyValue('height'));
-            if (document.getElementById("toppage-toggle").checked && Math.ceil(el.scrollTop + height) < el.scrollHeight) {
+            let el = document.getElementById("contents-body");
+            let height = parseInt(
+                window.getComputedStyle(el).getPropertyValue("height")
+            );
+            if (
+                document.getElementById("toppage-toggle").checked &&
+                Math.ceil(el.scrollTop + height) < el.scrollHeight
+            ) {
                 return;
             }
             //document.getElementById("toppage-toggle").checked = !document.getElementById("toppage-toggle").checked;
-            document.getElementById('toppage-toggle').click();
-        } else if (e.keyCode == '38') {
-            let submenu = document.getElementsByClassName('submenu-active');
+            document.getElementById("toppage-toggle").click();
+        } else if (e.keyCode == "38") {
+            let submenu = document.getElementsByClassName("submenu-active");
             for (let c = 0; c < submenu.length; c++) {
                 if (submenu[c].checked) {
-                    document.querySelector('input[name="submenu-active"]:checked').checked = false;
+                    document.querySelector(
+                        'input[name="submenu-active"]:checked'
+                    ).checked = false;
                     submenuToggle(submenu[c].value, null);
                     return;
                 }
@@ -205,12 +266,18 @@
     };
 
     Barba.Pjax.start();
-    Barba.Dispatcher.on('linkClicked', function() {});
-    Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
+    Barba.Dispatcher.on("linkClicked", function () {});
+    Barba.Dispatcher.on("newPageReady", function (
+        currentStatus,
+        oldStatus,
+        container
+    ) {
         //eval(container.querySelector("script").innerHTML);
-        const category = container.attributes['x-category'].value;
-        document.getElementById('toppage-cnt').setAttribute('class', category);
-        document.querySelector('input[name="category-active"][value="' + category + '"]').checked = true;
+        const category = container.attributes["x-category"].value;
+        document.getElementById("toppage-cnt").setAttribute("class", category);
+        document.querySelector(
+            'input[name="category-active"][value="' + category + '"]'
+        ).checked = true;
         document.getElementById("contents-body").scrollTop = 0;
         updateSocialShare();
         pourContactEmailAddress();
@@ -219,32 +286,43 @@
 
     const categoryColors = {};
 
-    const col = document.getElementsByClassName('menu-line');
+    const col = document.getElementsByClassName("menu-line");
     for (let c = 0; c < col.length; c++) {
-        let color = window.getComputedStyle(col[c]).getPropertyValue('background-color');
-        let category = col[c].parentNode.querySelector('label').attributes['x-category'].value;
+        let color = window
+            .getComputedStyle(col[c])
+            .getPropertyValue("background-color");
+        let category = col[c].parentNode.querySelector("label").attributes[
+            "x-category"
+        ].value;
         categoryColors[category] = color;
     }
-    let links = document.querySelectorAll('a[href]');
-    const cbk = function(e) {
+    let links = document.querySelectorAll("a[href]");
+    const cbk = function (e) {
         if (e.currentTarget.href === window.location.href) {
             e.preventDefault();
             e.stopPropagation();
         }
     };
     for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', cbk);
+        links[i].addEventListener("click", cbk);
     }
     let onLoadFn = () => {
         let times = document.querySelectorAll(".timeUTC");
         times.forEach((t) => {
             let st = moment(t.innerHTML);
             let utc = moment.utc(t.innerHTML);
-            let date = st.format("DD") == utc.format("DD") ? "" : utc.format("ddd, MMM Do, ");
-            t.innerHTML = st.format("dddd, MMMM Do YYYY, h:mm a") + " [" + date + utc.format("h:mm a") + " UTC]";
-
+            let date =
+                st.format("DD") == utc.format("DD")
+                    ? ""
+                    : utc.format("ddd, MMM Do, ");
+            t.innerHTML =
+                st.format("dddd, MMMM Do YYYY, h:mm a") +
+                " [" +
+                date +
+                utc.format("h:mm a") +
+                " UTC]";
         });
     };
-    Barba.Dispatcher.on('transitionCompleted', onLoadFn);
+    Barba.Dispatcher.on("transitionCompleted", onLoadFn);
     document.addEventListener("DOMContentLoaded", onLoadFn);
 })();
